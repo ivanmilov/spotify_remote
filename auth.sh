e46f03ea7903e3ddcbd0b104fc43bc2f7764c8a8
Ivan Milov <522518+ivanmilov@users.noreply.github.com>
2020-07-23 11:13:51 +0200

    whitespaces fix

--- auth.sh
+++ auth.sh
@@ -11,23 +11,23 @@ CLIENT_ID=$(cat CLIENT_ID)
 while getopts ":a:t" opt
 do
     case ${opt} in
-        a) 
-			chromium-browser "https://accounts.spotify.com/authorize?client_id=$CLIENT_ID&redirect_uri=$REDIRECT&response_type=code&scope=$SCOPE"
-			sleep 2
-			read -p "Enter access code: " ACCESS_CODE
-			echo $ACCESS_CODE | tr -d '\n' > ACCESS_CODE
-			;;
-		t) 
-			echo "This option needs to be finished"
-			exit 
-			res=$(curl -s \
-			-H "Authorization: Basic $BASE64APPTOKEN" \
-			-d grant_type=authorization_code \
-			-d code="$ACCESS_CODE" \
-			-d redirect_uri=$REDIRECT \
-			https://accounts.spotify.com/api/token)
-			;;
-			
+        a)
+            chromium-browser "https://accounts.spotify.com/authorize?client_id=$CLIENT_ID&redirect_uri=$REDIRECT&response_type=code&scope=$SCOPE"
+            sleep 2
+            read -p "Enter access code: " ACCESS_CODE
+            echo $ACCESS_CODE | tr -d '\n' > ACCESS_CODE
+            ;;
+        t)
+            echo "This option needs to be finished"
+            exit
+            res=$(curl -s \
+            -H "Authorization: Basic $BASE64APPTOKEN" \
+            -d grant_type=authorization_code \
+            -d code="$ACCESS_CODE" \
+            -d redirect_uri=$REDIRECT \
+            https://accounts.spotify.com/api/token)
+            ;;
+
         *) echo None;;
     esac
 done
